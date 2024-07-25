46d7b59ebb34cd4995f01aa63a8c9adbee4dbfe1
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-24 23:16:28 -0300

    feat: cria módulo de api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JobsModule } from './modules/jobs/jobs.module';
+import { ConfigModule } from '@nestjs/config';
+import { loadEnv } from './config/env';
+import { ApiModule } from './modules/api/api.module';
 
 @Module({
-  imports: [JobsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [loadEnv],
+    }),
+    ApiModule,
+    JobsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/modules/api/api.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { ApiService } from './api.service';
+
+@Global()
+@Module({
+  providers: [ApiService],
+  exports: [ApiService],
+})
+export class ApiModule {}
